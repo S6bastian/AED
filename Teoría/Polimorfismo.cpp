354ad90ce1fb66bccca7a5630634474b147ce313
Sebastián Sánchez <seb2001sac@gmail.com>
2023-09-02 16:20:23 -0500

    v1.0.0 Totalmente funcional sin errores

--- Teoría/Polimorfismo.cpp
+++ Teoría/Polimorfismo.cpp
@@ -5,25 +5,10 @@ struct CP;
 struct CLess;
 struct CGreater;
 
-void bubble_sort_CLess(int* A, int size, CLess *l);
+void bubble_sort_CLess(int* A, int size, CLess *l); //Cambia el comportamiento usando polimorfismo
+void bubble_sort_CGreater(int* A, int size, CP *g);
 void swap(int* a, int* b);
 void print(int* A, int size);
-
-int main(){
-    
-    int A[] = {9,5,6,15,2,1,3,7,4,11,14,12,13};
-    int size = sizeof(A)/sizeof(int);
-    CLess* l;
-    CGreater* g;  
-
-    int* pA = A;
-    print(A, size);
-    bubble_sort_CLess(A, size, l);
-    print(A, size);
-
-
-    return 0;
-}
 
 struct CP{
     virtual bool cmp(int a, int b) = 0;
@@ -44,9 +29,20 @@ struct CGreater : CP{
 void bubble_sort_CLess(int* A, int size, CLess *l){
     
     for(int i = 0; i < size - 1; i++){
-        print(A, size);
         for(int j = 0; j < size - 1; j++){
             if(l->cmp(A[j+1], A[j])){
+                swap(A[j], A[j+1]);
+            }
+        }
+    }
+    print(A, size);
+}
+
+void bubble_sort_CGreater(int* A, int size, CP *g){ //Funciona si ponemos "CP* g" al igual que "CGreater* g"
+    
+    for(int i = 0; i < size - 1; i++){
+        for(int j = 0; j < size - 1; j++){
+            if(g->cmp(A[j], A[j+1])){
                 swap(A[j], A[j+1]);
             }
         }
@@ -65,4 +61,21 @@ void print(int* A, int size){
         cout<<A[i]<<" ";
     }
     cout<<endl;
+}
+
+int main(){
+    
+    int A[] = {9,5,6,15,2,1,3,7,4,11,14,12,13};
+    int B[] = {9,5,6,15,2,1,3,7,4,11,14,12,13};
+    int size = sizeof(A)/sizeof(int);
+    CLess* l = new CLess; // Si solo ponemos "CLess* l" no funciona
+    CGreater* g = new CGreater; // Si solo ponemos "CGreater* g" no funciona
+
+    print(A, size);
+    bubble_sort_CLess(A, size, l);
+    cout<<endl<<endl;
+    print(B, size);
+    bubble_sort_CGreater(B, size, g);
+
+    return 0;
 }
