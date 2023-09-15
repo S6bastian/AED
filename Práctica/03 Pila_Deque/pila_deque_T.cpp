38811a0cac4ab7ddde904a239f38cce65b46e2f6
Sebastián Sánchez <seb2001sac@gmail.com>
2023-09-15 13:05:41 -0500

    v1.1.0 - Añadida función print(), errores corregidos

--- Práctica/03 Pila_Deque/pila_deque_T.cpp
+++ Práctica/03 Pila_Deque/pila_deque_T.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
 using namespace std;
 
-template<class T, int size = 5>
+template<class T, int size_parameter = 5>
 struct nodo{
-    T valor;
+    int size = size_parameter;
     T* tia; //top inside array
-    T array[size];
+    T array[size_parameter];
     nodo<T>* next;
-    nodo(T v, nodo<T>* n = nullptr){
-        valor = v;
+    nodo(nodo<T>* n = nullptr){
         next = n;
         tia = array;
     }
@@ -16,28 +15,25 @@ struct nodo{
 
 template<class T>
 class pila{
-    nodo<T>* top = nullptr; 
+    nodo<T>* top = nullptr;
 public:
     void push(T v);
     T pop();
+    nodo<T>* getTop() const{
+        return top;
+    }
 };
 
 template<class T>
 void pila<T>::push(T v){
-    if(top == nullptr || top->tia-4 == top->array){
-        top = new nodo(v, top);
+    if(top == nullptr || top->tia == top->array+(top->size)-1){ //Creación de una pila nueva o cuando se acaba el espacio en array
+        top = new nodo(top);
         *(top->tia) = v;
     }
-    
     else{
         top->tia++;
         *(top->tia) = v;
     }
-    /*
-    cout<<top<<endl;
-    cout<<top->tia<<endl;
-    cout<<top->array<<endl;
-    */
 }
 
 template<class T>
@@ -56,13 +52,58 @@ T pila<T>::pop(){
     return tmp;
 }
 
+
+template<class T>
+void print(const pila<T>& P){
+    nodo<T>* currentTop = P.getTop();
+    T* currentTia = currentTop->tia;
+    //currentTia = currentTop->array+currentTop->size-1; // en caso de mostrar los array completos
+    if(!(currentTia == currentTop->array+currentTop->size-1)){
+        cout<<" -----> "<<"||";
+    }
+
+    while(1){
+        if(currentTia == currentTop->array+currentTop->size-1){
+            cout<<" -----> "<<"||";
+        }
+        cout<<" "<<*(currentTia);
+        if(currentTia == currentTop->array){
+            cout<<" "<<"||";
+            if(currentTop->next == nullptr && currentTia == currentTop->array){
+                cout<<endl;
+                break;
+            }
+            else{
+                currentTop = currentTop->next;
+                currentTia = currentTop->tia;
+            }
+        }
+        else{
+            currentTia--;
+        }
+    }
+    cout<<endl;
+}
+
+
 int main(){
     pila<int> P;
-
-    for(int i = 0; i < 6; i++){
+    int size = 53;
+    // Creación de Pila
+    for(int i = 0; i < size; i++){
         P.push(i);
     }
-    cout<<P.pop();
+
+    print<int>(P);
+    
+    // Destrucción 
+    cout<<"Popped elements:";
+    for(int i = 0; i < 25; i++){
+        cout<<" "<<P.pop();
+    }
+    cout<<endl<<endl;
+
+    print<int>(P);
     
     return 0;
 }
