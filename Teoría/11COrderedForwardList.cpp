19e80ccd3894834e419b31efb9d3a79e8c02baa8
Sebastián Sánchez <seb2001sac@gmail.com>
2023-09-30 21:42:34 -0500

    v1.0.0 falta implementar el método print()

--- Teoría/11COrderedForwardList.cpp
+++ Teoría/11COrderedForwardList.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 template<class T>
 struct CNode{
-    CNode* next;
+    CNode<T>* next;
     T v;
     CNode(T x){
         v = x;
@@ -15,20 +15,74 @@ template<class T>
 class CList{
     CNode<T>* head;
 public:
-    CList(){
-        head = nullptr;
-    }
-    bool find(T x, CNode<T>** &p);
-    bool insert(T x);
-    bool remove(T x);
+    CList();
+    ~CList();
+    bool Find(T x, CNode<T>** &p);
+    bool Insert(T x);
+    bool Remove(T x);
 };
 
 template<class T>
-bool CList<T> :: find(T x, CNode<T>** &p){
-    for (p = &head; *p && (*p)->v <x; p = &((*p)->next))
+CList<T>::CList(){
+    head = nullptr;
+}
+
+template<class T>
+CList<T>::~CList(){
+    for(CNode<T>* t = head; head; t = head){
+        head = head->next;
+        delete t;
+    }
+}
+
+template<class T>
+bool CList<T>::Find(T x, CNode<T>** &p){
+    for (p = &head; *p && (*p)->v < x; p = &((*p)->next))
     
     return *p && (*p)->v == x;
 }
 
 template<class T>
-bool CList
+bool CList<T>::Insert(T x){
+    CNode<T>** p;
+    
+    if(Find(x,p))
+        return false;
+    
+    CNode<T>* t = new CNode(x);
+    t->next = *p;
+    *p = t;
+
+    return true;
+}
+
+template<class T>
+bool CList<T>::Remove(T x){
+    CNode<T>** p;
+
+    if(!Find(x,p))
+        return false;
+
+    CNode<T>* t = *p;
+    *p = t->next;
+    delete t;
+    return 1;
+}
+
+
+
+int main(){
+    CList<int> List;
+    
+    //Insert
+    for(int i = 0; i < 17; i++){
+        List.Insert(i);
+    }
+
+    //Remove
+    for(int i = 1; i < 17; i+=2){
+        List.Remove(i);
+    }
+
+   return 0; 
+}
