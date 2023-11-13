5548be6260722a8615f2d2e2d392c0d9d23a33e4
Sebastián Sánchez <seb2001sac@gmail.com>
2023-11-12 20:54:24 -0500

    v0.0.2

--- Práctica/Red-Black_Tree/Red-Black_Tree.cpp
+++ Práctica/Red-Black_Tree/Red-Black_Tree.cpp
@@ -1,3 +1,11 @@
+/*
+	Notas:
+	1. Hecho en base al video: https://www.youtube.com/playlist?list=PL9xmBV_5YoZNqDI8qfOZgzbqahCUmUEin
+	2. root va a cambiar si se le hace una rotación (caso 3)
+	3. Se modifica los colores de otras ramas?
+*/
+
+
 #include <iostream>
 using namespace std;
 
@@ -6,12 +14,12 @@ struct Node {
 	T value;
 	Node* nodes[2];
 	Node* parent;
-	bool color; // 0 -> black and 1 -> red
+	bool color; // 0 -> negro y 1 -> rojo
 
 	Node(T v, *p = nullptr) {
 		value = v;
 		nodes[0] = nodes[1] = 0;
-		color = 1;		// always red when its created
+		color = 1;		// siempre rojo al crearse
 		parent = p;
 	}
 };
@@ -27,10 +35,10 @@ public:
 	bool Insert(T x);
 	bool Remove(T x);
 
-	void leftRotate(Node<T>**& p);
-	void rightRotate(Node<T>**& p);
-	void insertFixUp(Node<T>** p);
-	Node<T>** getSuccessor(Node<T>** q);
+	void LeftRotate(Node<T>**& p);
+	void RightRotate(Node<T>**& p);
+	void InsertFixUp(Node<T>** p);
+	Node<T>** GetSuccessor(Node<T>** q);
 
 private:
 	Node<T>* root;
@@ -62,31 +70,56 @@ bool RBTree<T>::Insert(T x) {
 
 	*p = new Node<T>(x, parent);
 
-	insertFixUp(p);
+	InsertFixUp(p);
 
 	return 1;
 }
 
 template <class T>
-void RBTree<T>::insertFixUp(Node<T>** p) {
+void RBTree<T>::InsertFixUp(Node<T>** p) {
 	Node<T>* parent = nullptr;
 	Node<T>* grandParent = nullptr;
+	Node<T>* uncle = nullptr;
+
+	// Caso 0
+	if (*p == root) {
+		root->color = 0;
+		return;
+	}
+
+	parent = (*p)->parent;
+	grandParent = parent->parent;
+	uncle = grandParent->nodes[grandParent->nodes[0] == parent]
+	/*
+	if (grandParent->nodes[0] == parent) uncle = grandParent->nodes[1];
+	else uncle = grandParent->nodes[0];
+	*/
+
+	// Caso 1
+	if (uncle->color == 1) {
+		parent->color = !(parent->color);
+		grandParent->color = !(grandParent->color);
+		uncle->color = !(uncle->color);
+	}
+
+	// Caso 2
+	if (uncle->color == 0 && ) {
+
+	}
 
 	while (*p != root && (*p)->color == 1) {
 
 	}
 
-	// Case 0
-	
 }
 
 template <class T>
-bool RBTree<T>::Remove(T x) {
+bool RBTree<T>::Remove(T x) { //FALTA IMPLEMENTAR PARA RBTREE
 	Node<T>** p;
 	if (!Find(x, p))
 		return 0;
 	if ((*p)->nodes[0] && (*p)->nodes[1]) {
-		Node<T>** q = getSuccessor(p);
+		Node<T>** q = GetSuccessor(p);
 		(*p)->value = (*q)->value;
 		p = q;
 	}
@@ -97,7 +130,7 @@ bool RBTree<T>::Remove(T x) {
 }
 
 template <class T>
-void RBTree<T>::leftRotate(Node<T>**& p){
+void RBTree<T>::LeftRotate(Node<T>**& p){
 	Node<T>** t = p;
 	p = &((*p)->nodes[1]);
 	(*p)->parent = (*t)->parent;
@@ -110,7 +143,7 @@ void RBTree<T>::leftRotate(Node<T>**& p){
 }
 
 template <class T>
-void RBTree<T>::rightRotate(Node<T>**& p) {
+void RBTree<T>::RightRotate(Node<T>**& p) {
 	Node<T>** t = p;
 	p = &((*p)->nodes[0]);
 	(*p)->parent = (*t)->parent;
@@ -123,9 +156,9 @@ void RBTree<T>::rightRotate(Node<T>**& p) {
 }
 
 template <class T>
-Node<T>** RBTree<T>::getSuccessor(Node<T>** q) {
-	bool side = 1;	// closest smallest = 0		(left)
-					// closest largest = 1		(right)
+Node<T>** RBTree<T>::GetSuccessor(Node<T>** q) {
+	bool side = 1;	// menor más cercano = 0		(left)
+					// mayor más cercano = 1		(right)
 	q = &((*q)->nodes[side]);
 	side = !side;
 	while ((*q)->nodes[side])
